73406513b4a6d516062a877eed84df711451f4ff
kayvank <kayvan@q2io.com>
2026-03-10 08:15:19 -0700

    fix alt/ctrl keys
    In terminal modes, these keys are not translated well and cause issues
    in particular in emacs org mode

--- modules/users/programs/cli/kitty/default.nix
+++ modules/users/programs/cli/kitty/default.nix
@@ -1,12 +1,13 @@
 { config, lib, pkgs, ... }: {
   programs.kitty = {
     enable = true;
+    settings = {
+      macos_option_as_alt = "yes"; ## fix the `Alt` and `Ctrl` key in terminal mode with emacs orgmode
+      font_family = "Mono Book";
+      font_size = 13;
+      enable_audio_bell = false;
+    };
     shellIntegration.enableZshIntegration = true;
 
-      extraConfig = ''
-        font_family Mono Book
-        font_size 13
-        enable_audio_bell = false;
-'';
   };
 }
